734b1a50f279d9160f2a15c8a404fe9753273fe2
amiradediran <amiradediran@gmail.com>
2019-12-16 16:43:21 +0100

    Fix test in app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders h1', () => {
   const { getByText } = render(<App />);
   const h1Element = getByText(/Welcome to our nameless Workout Tracker/i);
   expect(h1Element).toBeInTheDocument();
