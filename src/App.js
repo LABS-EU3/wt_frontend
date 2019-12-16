2741ac688f15a4720c5fd3824f07d15a62913c43
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-12-16 17:27:35 +0100

    Configure linting and husky

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
         <h1>Welcome to our nameless Workout Tracker</h1>
-        
       </header>
     </div>
   );
