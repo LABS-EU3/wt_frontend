ce84a659176bbf4b6ca9cac92bf4def68ef6ebd0
Modurotolu Olokode <durolawk@gmail.com>
2019-12-17 12:07:16 +0100

    intialized apollo client on frontend

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+    uri: ``
+});
+
+const client = new ApolloClient({
+    cache,
+    link
+})
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
