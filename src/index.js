517031a9217e6a7c0e297dd9ce27a9f4f5a090f1
Modurotolu Olokode <durolawk@gmail.com>
2019-12-17 12:09:42 +0100

    connected apollo to react application

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
+import { ApolloProvider } from '@apollo/react-hooks'
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
@@ -17,7 +18,7 @@ const client = new ApolloClient({
     link
 })
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ApolloProvider client ={client}><App /></ApolloProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
