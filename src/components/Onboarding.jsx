bbb4a5af6f2d42a6918e01f9b779e043fd173343
Modurotolu Olokode <durolawk@gmail.com>
2019-12-17 19:33:59 +0100

    implemented button design for unit selection

--- src/components/Onboarding.jsx
+++ src/components/Onboarding.jsx
@@ -1,6 +1,17 @@
 import React from "react";
 import { Flex } from "@chakra-ui/core";
 import { Heading } from "@chakra-ui/core";
+import { Button } from "@chakra-ui/core";
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuGroup,
+  MenuDivider,
+  MenuOptionGroup,
+  MenuItemOption
+} from "@chakra-ui/core";
 
 function Onboarding() {
   return (
@@ -10,6 +21,21 @@ function Onboarding() {
       </div>
       <div>
         <Heading size="lg">Preferences</Heading>
+        <p>Which weight measurement unit do you prefer?</p>
+        <Button variantColor="orange" variant="outline">
+          kilogram
+        </Button>
+        <Button variantColor="orange" variant="outline">
+          pounds
+        </Button>
+        <p>Which height measurement unit do you prefer?</p>
+        <Button variantColor="orange" variant="outline">
+          meters
+        </Button>
+        <Button variantColor="orange" variant="outline">
+          inches
+        </Button>
+        <p>What is your fitness goal?</p>
       </div>
     </Flex>
   );
