cfe939c03ad344662d42a9380839c522035cde90
Folasade Agbaje <folasadeagbaje@gmail.com>
2020-01-06 12:01:52 +0100

    update spinner to be centralised

--- src/components/common/Spinner.jsx
+++ src/components/common/Spinner.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { Spinner, Box } from "@chakra-ui/core";
 
-function CustomSpinner({ thickness, emptyColor, color, size }) {
+function CustomSpinner({ thickness, size, text }) {
   return (
     <Box>
       <Spinner
         thickness={thickness}
-        emptyColor={emptyColor}
-        color={color}
+        emptyColor="gray.200"
+        color="orange.400"
         size={size}
       />
+      {text ? <Box ml={3}>{text}</Box> : null}
     </Box>
   );
 }
