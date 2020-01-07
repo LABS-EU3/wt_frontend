a477c1dc195185268c85ea2d1df2d4a9bdcab7c7
amxra <amiradediran@gmail.com>
2020-01-07 17:28:32 +0100

    Create search bar for exercise list

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@ import PrivateRoute from "./components/common/PrivateRoute";
 import Login from "./components/auth/LogIn";
 import Dashboard from "./components/Dashboard";
 import SignUp from "./components/auth/SignUp";
+import Exercises from "./components/exercise/ExerciseList";
 
 function App() {
   return (
     <div className="App" data-testid="App">
       <Route exact path="/login" component={Login} />
+      {/* <Route exact path="/login" component={Exercises} /> */}
       <PrivateRoute exact path="/onboarding" component={Onboarding} />
       <Route exact path="/signup" component={SignUp} />
       <PrivateRoute exact path="/" component={Dashboard} />
---
+++ src/components/exercise/ExerciseStyle.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const ExercisesStyle = styled.div`
+  border: 1px solid red;
+  min-height: 100vh;
+  width: 80vw;
+  margin: 0 auto;
+
+  .search-container {
+    border: 1px solid red;
+    width: 70%;
+    text-align: left;
+
+    h4 {
+      font-weight: bold;
+    }
+
+    p {
+      color: grey;
+      padding: 0.2rem 0;
+    }
+  }
+`;
