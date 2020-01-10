af99a73a0ff12576a3d91d24dc435f91581bd795
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-01-10 09:01:57 +0100

    cleanup & refactor

--- src/components/workouts/WorkoutActionItems.jsx
+++ src/components/workouts/WorkoutActionItems.jsx
@@ -1,47 +1,54 @@
 import React from "react";
 import { Button, ButtonGroup } from "@chakra-ui/core";
 import { FaPlayCircle, FaStopCircle, FaCircle, FaPause } from "react-icons/fa";
+import styled from "styled-components";
+
+const StyledWorkoutItems = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
 
 const WorkoutActionItems = ({}) => {
   return (
-    <div>
+    <StyledWorkoutItems>
       <ButtonGroup spacing={4} textAlign="left" marginY="30px">
         <Button
           rightIcon={FaCircle}
           variantColor="blue"
           variant="outline"
-          size="lg"
+          size="md"
         >
           Schedule
-        </Button>
-
-        <Button
-          rightIcon={FaPause}
-          variantColor="orange"
-          variant="outline"
-          size="lg"
-        >
-          Pause
         </Button>
 
         <Button
           rightIcon={FaPlayCircle}
           variantColor="green"
           variant="solid"
-          size="lg"
+          size="md"
         >
           Start
         </Button>
+
+        <Button
+          rightIcon={FaPause}
+          variantColor="orange"
+          variant="outline"
+          size="md"
+        >
+          Pause
+        </Button>
+
         <Button
           rightIcon={FaStopCircle}
           variantColor="red"
           variant="outline"
-          size="lg"
+          size="md"
         >
           Stop
         </Button>
       </ButtonGroup>
-    </div>
+    </StyledWorkoutItems>
   );
 };
 
