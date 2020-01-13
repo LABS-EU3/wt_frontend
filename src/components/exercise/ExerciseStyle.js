cbf7a6e58fb6dc401b77675bb496e398633ecc59
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-01-13 17:58:31 +0100

    style workout detail

--- src/components/exercise/ExerciseStyle.js
+++ src/components/exercise/ExerciseStyle.js
@@ -9,6 +9,10 @@ export const ExercisesStyle = styled.div`
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
   padding: 2rem 0;
 
+  @media only screen and (max-width: 500px) {
+    width: 95vw;
+  }
+
   .search-container {
     border: 1px solid red;
     width: 70%;
@@ -125,6 +129,7 @@ export const ExerciseStyle = styled.div`
   margin: 1rem;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
   transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+  justify-content: center;
 
   &:hover {
     box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
@@ -137,6 +142,10 @@ export const ExerciseStyle = styled.div`
 
   @media only screen and (max-width: 800px) {
     width: 75%;
+  }
+
+  @media only screen and (max-width: 500px) {
+    width: 95%;
   }
 
   img {
