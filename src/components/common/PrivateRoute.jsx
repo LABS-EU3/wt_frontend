02bd02a31f77b9b41e739906d218fb0b8b0358ea
Folasade Agbaje <folasadeagbaje@gmail.com>
2020-01-20 15:41:15 +0100

    modified private route

--- src/components/common/PrivateRoute.jsx
+++ src/components/common/PrivateRoute.jsx
@@ -1,13 +1,31 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
-import { isLoggedIn } from "../../utils";
+import { isLoggedIn, userOnboardedSuccessfully } from "../../utils";
 
+const isValidPath = path => {
+  if (
+    path === "/" ||
+    path === "/login" ||
+    path === "/signup" ||
+    path === "/onboarding"
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+};
 const PrivateRoute = ({ component: Component, ...rest }) => {
   const isSignedIn = isLoggedIn();
   if (isSignedIn) {
-    return <Route {...rest} render={props => <Component {...props} />} />;
+    const onBoarded = userOnboardedSuccessfully();
+    if (onBoarded || isValidPath(rest.path))
+      return <Route {...rest} render={props => <Component {...props} />} />;
+    else {
+      return <Redirect to="/onboarding" />;
+    }
+  } else {
+    return <Redirect to="/login" />;
   }
-  return <Redirect to="/login" />;
 };
 
 export default PrivateRoute;
