39413ffa7ba4c7c4da41d853876967fc6b03060a
Folasade Agbaje <folasadeagbaje@gmail.com>
2020-01-28 13:38:26 +0100

    modified style for buddiescard

--- src/components/buddies/BuddiesCard.jsx
+++ src/components/buddies/BuddiesCard.jsx
@@ -1,4 +1,4 @@
-import { Avatar, Box, Flex, Heading, Text } from "@chakra-ui/core";
+import { Avatar, Box, Flex, Heading, Text, Divider } from "@chakra-ui/core";
 import React from "react";
 import { withApollo } from "react-apollo";
 import CustomButtons from "./CustomButtons";
@@ -9,24 +9,28 @@ const BuddiesCard = ({
   goal,
   history,
   text,
-  profilePicture,
-  icon
+  icon,
+  variant,
+  onClick,
+  profilePicture
 }) => {
   return (
-    <Box
-      boxShadow="0px 2px 6px 0px rgba(0, 0, 0, 0.12)"
-      paddingY="15px"
-      margin="30px"
-    >
-      <Flex alignItems="center" justifyContent="space-between" margin="0 30px">
-        <Flex justifyContent="space-around" alignItems="center">
+    <Box>
+      <Divider borderColor="gray.300" />
+      <Flex alignItems="center" justifyContent="space-between">
+        <Flex justifyContent="space-around" alignItems="center" paddingY="5px">
           <Avatar src={profilePicture} size="xl" />
           <Box textAlign="left" marginLeft="30px">
             <Heading size="md">{name}</Heading>
             <Text>{goal}</Text>
           </Box>
         </Flex>
-        <CustomButtons icon={icon} text={text} />
+        <CustomButtons
+          icon={icon}
+          text={text}
+          variant={variant}
+          onClick={onClick}
+        />
       </Flex>
     </Box>
   );
