9cb9a1776f5e2c65d6ef52acf8d69cba70bf03e2
Modurotolu Olokode <durolawk@gmail.com>
2020-01-28 11:38:42 +0100

    connected friends search and suggested to backend

--- src/components/buddies/AddFriendsTab.jsx
+++ src/components/buddies/AddFriendsTab.jsx
@@ -14,12 +14,13 @@ import {
   Text,
   icon
 } from "@chakra-ui/core";
+import { Redirect } from "react-router-dom";
 
 import logoImage from "../../images/login_image.png";
 import BuddiesCard from "./BuddiesCard";
 import Search from "../common/Search";
-import { GET_UNITS } from "../../graphql/queries";
-import CustomButtons from "./CustomButtons";
+import CustomSpinner from "../common/Spinner";
+import { GET_USERS } from "../../graphql/queries";
 
 const AddFriendsTab = ({
   client,
@@ -30,35 +31,75 @@ const AddFriendsTab = ({
   profilePicture
 }) => {
   const [buddiesData, setBuddiesData] = useState([]);
-  // const [isLoading, setIsLoading] = useState(true);
-  // const toast = useToast();
+  const [isLoading, setIsLoading] = useState(true);
+  const toast = useToast();
+  const [search, setSearch] = useState("");
+  const [error, setError] = useState(false);
+
+  const alert = (title, description, status) => {
+    toast({
+      title,
+      description,
+      status,
+      duration: 9000,
+      isClosable: true
+    });
+  };
 
   useEffect(() => {
     client
       .query({
-        query: GET_UNITS
+        query: GET_USERS,
+        variables: {
+          search,
+          fields: ["firstname", "lastname", "email"]
+        }
       })
       .then(res => {
-        setBuddiesData(res.data.units);
-        // setIsLoading(false);
+        setBuddiesData(res.data.findFriends);
+        setIsLoading(false);
       })
       .catch(err => {
-        console.log(err);
-        // setIsLoading(false);
+        setIsLoading(false);
         alert("An error occurred.", "Unable to load", "error");
+        setError(true);
       });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [search]);
+
+  if (isLoading) {
+    return (
+      <Box>
+        <Flex
+          width="100vw"
+          height="100vh"
+          justifyContent="center"
+          align="center"
+        >
+          <CustomSpinner thickness="6px" size="xl" text="Loading..." />
+        </Flex>
+      </Box>
+    );
+  }
+
+  if (error) {
+    alert("An error occurred.", "Unable to load workouts", "error");
+    return <Redirect to="/" />;
+  }
 
   return (
     <Box boxShadow="0px 2px 6px 0px rgba(0, 0, 0, 0.12)" paddingY="5px">
       <p>Add Friends</p>
-      <Search placeholder="" />
+      <Search
+        placeholder="Find someone you know"
+        setSearch={setSearch}
+        search={search}
+      />
       {buddiesData.map(buddy => (
         <div>
           <BuddiesCard
-            name={buddy.name}
-            goal={buddy.type}
+            name={`${buddy.firstname} ${!buddy.lastname ? "" : buddy.lastname}`}
+            goal={buddy.goal}
             icon="add"
             text="Add friend"
           />
