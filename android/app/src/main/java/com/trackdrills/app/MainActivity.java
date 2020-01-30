f034f9a77d7e24463959196cdd495b15c6120d4f
Melquisedeque Pereira <melqui1@live.com.pt>
2020-01-30 18:30:20 +0000

    Added missing import class google auth

--- android/app/src/main/java/com/trackdrills/app/MainActivity.java
+++ android/app/src/main/java/com/trackdrills/app/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
+import com.codetrixstudio.capacitor.GoogleAuth.GoogleAuth;
 
 import java.util.ArrayList;
 
