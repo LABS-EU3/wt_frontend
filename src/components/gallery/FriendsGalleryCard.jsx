23c89f568dff0aa70b391adbbebaab3ce805534c
Folasade Agbaje <folasadeagbaje@gmail.com>
2020-02-05 11:45:10 +0100

    installed the carousel packgae and created component

--- src/components/gallery/FriendsGalleryCard.jsx
+++ src/components/gallery/FriendsGalleryCard.jsx
@@ -1,9 +1,29 @@
 import React from "react";
-import { Stack, Box, Divider, Heading, Text } from "@chakra-ui/core";
+import { Stack, Box, Divider, Heading, Text, Image } from "@chakra-ui/core";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 import { FriendsGalleryStyle } from "./StyledGallery";
 
 const FriendsGallery = ({ wGallery }) => {
   const { gallery, firstname, lastname, goal } = wGallery;
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+      slidesToSlide: 3 // optional, default to 1.
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+      slidesToSlide: 2 // optional, default to 1.
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      slidesToSlide: 1 // optional, default to 1.
+    }
+  };
+
   return (
     <FriendsGalleryStyle>
       <Divider borderColor="gray.300" />
@@ -15,20 +35,28 @@ const FriendsGallery = ({ wGallery }) => {
           Goal: <span>{goal}</span>
         </Text>
       </section>
-      <Stack isInline className="stack">
+
+      <Carousel responsive={responsive}>
         {gallery.map((gal, indx) => {
-          while (indx < 3) {
-            return (
-              <Box key={gal.id} className="box">
-                <img src={gal.picture} alt="user-progress" />
+          return (
+            <div>
+              <Image
+                src={gal.picture}
+                alt="user-progress"
+                height="240px"
+                width="400px"
+                paddingX={3}
+              />
+              <Box textAlign="left" paddingLeft={3}>
                 <Text>{gal.workoutId.name}</Text>
-                <Text>{new Date(gal.endDate).toLocaleDateString()}</Text>
+                <Text color="orange.400">
+                  {new Date(gal.endDate).toLocaleDateString()}
+                </Text>
               </Box>
-            );
-          }
-          return null;
+            </div>
+          );
         })}
-      </Stack>
+      </Carousel>
     </FriendsGalleryStyle>
   );
 };
